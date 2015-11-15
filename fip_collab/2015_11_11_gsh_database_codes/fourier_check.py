d82d89efde2e814c04557ddc1278109487fe219f
Noah Paulson <noahhpaulson@gmail.com>
2015-11-15 11:24:30 -0500

    first draft codes for interpolation using multidimensional fourier representation

---
+++ fip_collab/2015_11_11_gsh_database_codes/fourier_check.py
@@ -0,0 +1,70 @@
+import numpy as np
+import numpy.polynomial.legendre as leg
+import gsh_hex_tri_L0_8 as gsh
+import h5py
+import time
+
+"""
+This script checks the accuracy of the predictions
+of the calibrated fourier series approximating the
+crystal plasticity solutions
+"""
+
+# define the number of increments for angular variables:
+
+n_p1 = 11  # number of phi1 samples
+n_P = 11  # number of Phi samples
+n_p2 = 11  # number of phi2 samples
+n_en = 100  # number of en samples
+
+n_par = n_p1*n_P*n_p2*n_en
+
+f_test = h5py.File('test_fourier.hdf5', 'r')
+ep_set = f_test.get("ep_set")
+
+theta = ep_set[:, 0]
+e_angles = ep_set[:, 1:4]
+et_norm = ep_set[:, 4]
+Y = ep_set[:, 5]
+
+f_coeff = h5py.File('fourier_coeff.hdf5', 'r')
+coeff_vec = f_coeff.get('coeff_vec')
+
+# N_L = np.max(coeff_vec[:,0])+1
+# N_p = np.max(coeff_vec[:,1])+1
+# N_q = np.max(coeff_vec[:,2])+1
+
+interp_vec = np.zeros(ep_set.shape[0])
+
+st = time.time()
+
+for ii in xrange(coeff_vec.shape[0]):
+	L, p, q = coeff_vec[ii, :3]
+
+    p_vec = np.zeros(p+1)
+    p_vec[p] = 1
+
+    vec = gsh.gsh(e_angles, c) * \
+          leg.legval(et_norm, p_vec) * \
+          np.exp((1j*2*np.pi*q*theta)/L)
+
+    interp_vec += vec
+
+end = time.time()
+n_interp = ep_set.shape[0]
+time_per_interp = np.round((end-st)/n_interp, 7)
+
+print "interpolation time per input set: %ss" % time_per_interp
+
+error = np.abs(Y - interp_vec)*1E6
+
+print "mean interpolation error: %s (ep in ppm)" % np.mean(error)
+print "max interpolation error: %s (ep in ppm)" % np.max(error)
+
+print "worst case prediction error:"
+max_indx = np.argmax(error)
+print "actual strain: %s" % Y[max_indx]
+print "interpolated strain: %s" % interp_vec[max_indx]
+
+f_test.close()
+f_coeff.close()
--- fip_collab/2015_11_11_gsh_database_codes/read_check_data.py
+++ fip_collab/2015_11_11_gsh_database_codes/read_check_data.py
@@ -1,35 +1,35 @@
 import numpy as np
 import h5py
 import sys
+import leg_interp_func as lif
 
 """
 in this version of the code the id of the tensor is an argument to
 the script.
+
+the script saves the magnitudes of the roots for the legendre polynomial
 """
 
 # initialize important variables
 
 tnum = sys.argv[1]
 
-# define the number of increments for angular variables:
+inc = 1
 
-inc = 3  # degree increment for angular variables
+n_p1 = 11  # number of phi1 samples
+n_P = 11  # number of Phi samples
+n_p2 = 11  # number of phi2 samples
+n_en = 100  # number of en samples
 
-n_th = (60/inc)+1  # number of theta samples for FZ
-n_p1 = 360/inc  # number of phi1 samples for FZ
-n_P = (90/inc)+1  # number of Phi samples for FZ
-n_p2 = 60/inc  # number of phi2 samples for FZ
-n_en = 100  # number
-
-n_eul = n_p1 * n_P * n_p2
+n_eul = n_p1*n_P*n_p2
 
 # create file for pre-database outputs
-f_nhp = h5py.File('coeff_extract_%s.hdf5' % str(tnum).zfill(2), 'w')
+f_nhp = h5py.File('test_extract_%s.hdf5' % str(tnum[1]).zfill(2), 'w')
 ep_set = f_nhp.create_dataset("ep_set",
                               (n_eul*n_en, 6))
 
 # Read Simulation info from "sim" file
-filename = 'sim_Ti64_tensor_%s.txt' % str(tnum).zfill(2)
+filename = 'sim_Ti64_tensor_%s.txt' % str(tnum[1]).zfill(2)
 
 f = open(filename, "r")
 
@@ -84,11 +84,6 @@ for ii in xrange(0, n_eul):
     # calculate the norm of et
     et_norm = np.sqrt(et[:, 0]**2 + et[:, 1]**2 + et[:, 2]**2 +
                       2*(et[:, 3]**2 + et[:, 4]**2 + et[:, 5]**2))
-
-    err = np.max(np.abs(et_norm-et_vec))
-    if err > max_err:
-        print err
-        max_err = err
 
     ep11 = ep[:, 0]
 
