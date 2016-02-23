271d769d9f6aadb5dba190265ea72a755d3ed69c
Noah Paulson <noahhpaulson@gmail.com>
2016-02-23 18:36:59 -0500

    dumb mistake

--- fip_collab/2016_02_18_full_study/main_val.py
+++ fip_collab/2016_02_18_full_study/main_val.py
@@ -44,47 +44,47 @@ for step in xrange(5, 6):
     """
 
     """Gather data from validation vtk files"""
-    # tensorID = 1
+    tensorID = 1
 
-    # vtk.read_euler(el, ns_val, set_id_val, step, dir_val, wrt_file, tensorID)
+    vtk.read_euler(el, ns_val, set_id_val, step, dir_val, wrt_file, tensorID)
 
-    # for comp in compl:
-    #     vtk.read_meas(el, ns_val, set_id_val, step, comp, tensorID,
-    #                   dir_val, wrt_file)
+    for comp in compl:
+        vtk.read_meas(el, ns_val, set_id_val, step, comp, tensorID,
+                      dir_val, wrt_file)
 
-    # vtk.read_fip(el, ns_val, set_id_val, step, dir_val, wrt_file)
+    vtk.read_fip(el, ns_val, set_id_val, step, dir_val, wrt_file)
 
-    # """Convert the orientations from the calibration datasets from bunge euler
-    # angles to GSH coefficients"""
-    # gsh.euler_to_gsh(el, H, ns_val, set_id_val, step, wrt_file)
+    """Convert the orientations from the calibration datasets from bunge euler
+    angles to GSH coefficients"""
+    gsh.euler_to_gsh(el, H, ns_val, set_id_val, step, wrt_file)
 
-    # """Perform the validation"""
-    # for comp in compl:
-    #     validation.validation(el, H, ns_cal, ns_val, set_id_cal, set_id_val,
-    #                           step, comp, typ, wrt_file)
+    """Perform the validation"""
+    for comp in compl:
+        validation.validation(el, H, ns_cal, ns_val, set_id_cal, set_id_val,
+                              step, comp, typ, wrt_file)
 
-    # """Get error metrics for each total strain component"""
-    # newID = 'rmks'
-    # traID = 'r'
-    # for comp in compl:
-    #     rtex.results(el, ns_val, set_id_val, step, typ, comp,
-    #                  newID, traID, nfac)
+    """Get error metrics for each total strain component"""
+    newID = 'rmks'
+    traID = 'r'
+    for comp in compl:
+        rtex.results(el, ns_val, set_id_val, step, typ, comp,
+                     newID, traID, nfac)
 
-    # """Calculate FIP fields"""
-    # fipmat = np.zeros((ns_val, el**3))
+    """Calculate FIP fields"""
+    fipmat = np.zeros((ns_val, el**3))
 
-    # for sn in xrange(ns_val):
-    #     fipmat[sn, :] = fip.fip(sn, el, ns_val, set_id_val, step, typ, compl)
+    for sn in xrange(ns_val):
+        fipmat[sn, :] = fip.fip(sn, el, ns_val, set_id_val, step, typ, compl)
 
-    # f = h5py.File("fip_%s%s_s%s.hdf5" % (ns_val, set_id_val, step), 'a')
-    # f.create_dataset('fipmks', data=fipmat)
-    # f.close()
+    f = h5py.File("fip_%s%s_s%s.hdf5" % (ns_val, set_id_val, step), 'a')
+    f.create_dataset('fipmks', data=fipmat)
+    f.close()
 
-    """Perform Blurring on FIPs"""
-    parID = 'fip'
-    field_blur.blur(el, ns_val, set_id_val, step, parID)
-    fs.fip_sort(el, ns_val, set_id_val, step, parID)
+    # """Perform Blurring on FIPs"""
+    # parID = 'fip'
+    # field_blur.blur(el, ns_val, set_id_val, step, parID)
+    # fs.fip_sort(el, ns_val, set_id_val, step, parID)
 
-    parID = 'fipmks'
-    field_blur.blur(el, ns_val, set_id_val, step, parID)
-    fs.fip_sort(el, ns_val, set_id_val, step, parID)
+    # parID = 'fipmks'
+    # field_blur.blur(el, ns_val, set_id_val, step, parID)
+    # fs.fip_sort(el, ns_val, set_id_val, step, parID)
